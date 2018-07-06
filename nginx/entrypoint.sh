1b9d5a3dbca48b39c12f29f57d87d53c696f8457
lintmx <lintmx@gmail.com>
2018-07-07 01:38:27 +0800

    support multiple php version

---
+++ nginx/entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+rm -rf /etc/nginx/conf.d/*.conf
+
+for line in $(cat /config | sed '/^#.*$/d' | tr -s '\n')
+do
+    project_name=`echo -n "$line" | awk -F ',' '{printf $1}'`
+    project_dir=`echo -n "$line" | awk -F ',' '{printf $2}'`
+    php_version=`echo -n "$line" | awk -F ',' '{printf $3}'`
+
+    cp /etc/nginx/conf.d/site.template /etc/nginx/conf.d/$project_name.conf
+    sed -i 's/<{project_domain}>/'$PROJECT_DOMAIN'/g' /etc/nginx/conf.d/$project_name.conf
+    sed -i 's/<{project_name}>/'$project_name'/g' /etc/nginx/conf.d/$project_name.conf
+    sed -i 's/<{project_dir}>/'$project_dir'/g' /etc/nginx/conf.d/$project_name.conf
+    sed -i 's/<{php_version}>/'$php_version'/g' /etc/nginx/conf.d/$project_name.conf
+done
+
+exec "$@"
