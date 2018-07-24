1009fdd1ec43a7458e53172001d3a5c7037d85b3
lintmx <lintmx@gmail.com>
2018-07-24 22:34:10 +0800

    check public directory in nginx entrypoint

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -11,8 +11,13 @@ do
     cp /etc/nginx/conf.d/site.template /etc/nginx/conf.d/$project_name.conf
     sed -i 's/<{project_domain}>/'$PROJECT_DOMAIN'/g' /etc/nginx/conf.d/$project_name.conf
     sed -i 's/<{project_name}>/'$project_name'/g' /etc/nginx/conf.d/$project_name.conf
-    sed -i 's/<{project_dir}>/'$project_dir'/g' /etc/nginx/conf.d/$project_name.conf
     sed -i 's/<{php_version}>/'$php_version'/g' /etc/nginx/conf.d/$project_name.conf
+
+    if [ -d "/www/$project_dir/public" ]; then
+        sed -i 's/<{project_dir}>/'$project_dir'\/public/g' /etc/nginx/conf.d/$project_name.conf
+    else
+        sed -i 's/<{project_dir}>/'$project_dir'/g' /etc/nginx/conf.d/$project_name.conf
+    fi
 done
 
 exec "$@"
